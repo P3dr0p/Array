8187d148cb4ab38a2db874e658e605e4f5388d4e
Pedro Paulo Ramalho de Almeida <108875939+P3dr0p@users.noreply.github.com>
2023-03-27 12:14:28 -0300

    Add files via upload

--- Array/js/script.js
+++ Array/js/script.js
@@ -1,5 +1,4 @@
-// Botão Adicionar
-
+// Botão Adicionar - Recuperar o valor contido no campo de texto.
 const btn = document.querySelector("#send");
 
 btn.addEventListener("click", function(e) {
@@ -13,4 +12,50 @@ btn.addEventListener("click", function(e) {
     console.log(value);
 });
 
-// Botão Ordenar
+// Botão Adicionar - Exibir Alerta
+
+function AbrirAlerta(){
+    alert("Informe um valor válido")
+}
+
+// Botão Adicionar - Verificar se o valor informado já existe dentro do Array.
+
+let objeto = ["Cadeira", "Impressora", "Garfo"];
+
+console.log(objeto.includes("Cadeira"));
+
+if(objeto.includes("Cadeira")) {
+    console.log("Objeto já foi adicionado")
+};
+
+if(objeto.includes("Impressora")){
+    console.log("Objeto já foi adicionado")
+};
+
+if(objeto.includes("Garfo")){
+    console.log("Objeto já foi adicionado")
+};
+
+// Botão Ordenar - Ordenar de Forma Alfabética os valores.
+
+var objetos = [
+    {
+        nome: "Cadeira",
+    },
+    {
+        nome: "Impressora"
+    },
+    {
+        nome: "Garfo"
+    },
+];
+
+objetos.sort(function(a, z) {
+    if(a.nome < z.nome) {
+        return -1;
+    } else {
+        return true;
+    }
+});
+
+console.log(objetos);
