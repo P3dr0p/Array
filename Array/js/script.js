f286189c1411c50cf423b7a54be78b294fbde8ba
Pedro Paulo Ramalho de Almeida <108875939+P3dr0p@users.noreply.github.com>
2023-03-27 10:54:52 -0300

    Add files via upload

---
+++ Array/js/script.js
@@ -0,0 +1,16 @@
+// Botão Adicionar
+
+const btn = document.querySelector("#send");
+
+btn.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    const name = document.querySelector("#name");
+
+    const value = name.value;
+
+    console.log(value);
+});
+
+// Botão Ordenar
